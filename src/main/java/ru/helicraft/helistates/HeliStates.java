f422c43718bf841e7308acf8e4211dd2246a8642
ms0ur <maksimsuslov7142@gmail.com>
2025-06-07 22:50:04 +0300

    Initial Commit

--- src/main/java/ru/helicraft/helistates/HeliStates.java
+++ src/main/java/ru/helicraft/helistates/HeliStates.java
@@ -1,11 +1,19 @@
 package ru.helicraft.helistates;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HeliStates extends JavaPlugin {
+    @Getter
+    private static HeliStates instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+        if(!getConfig().getBoolean("enabled")) return;
+
         getLogger().info("Enabled");
     }
 
